95fc2af1b4d5417be0937942979ec766ae9fe439
blockiosaurus <blockiosaurus@gmail.com>
2022-01-09 11:15:33 -0500

    Adding webpack and splitting out server.

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require("copy-webpack-plugin");
+var glob = require("glob");
+
+const ROOT = path.resolve(__dirname, 'src');
+const DESTINATION = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    context: ROOT,
+
+    entry: {
+        'main': './index.js',
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: DESTINATION
+    },
+
+    resolve: {
+        extensions: ['.ts', '.js'],
+        modules: [
+            ROOT,
+            'node_modules'
+        ]
+    },
+
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: 'assets', to: 'assets' },
+                { from: 'index.html', to: 'index.html'}
+            ]
+        })
+    ],
+
+    module: {
+        rules: [
+            /****************
+            * PRE-LOADERS
+            *****************/
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                use: 'source-map-loader'
+            },
+            {
+                enforce: 'pre',
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'tslint-loader'
+            },
+
+            /****************
+            * LOADERS
+            *****************/
+            {
+                test: /\.ts$/,
+                exclude: [/node_modules/],
+                use: 'awesome-typescript-loader'
+            }
+        ]
+    },
+
+    devtool: 'cheap-module-source-map',
+    devServer: {
+        historyApiFallback: true,
+        static: path.resolve(__dirname, './src'),
+        hot: true,
+        port: 8080
+    }
+};
+
